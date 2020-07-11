5e4ea1d59e625ce51780cae69429943b820698c1
Roger Lee <rogerlee0421@gmail.com>
2020-07-11 10:33:40 +0800

    Update douyin.go

--- douyin.go
+++ douyin.go
@@ -2,12 +2,17 @@ package douyin
 
 import "log"
 
-func WatermarkRemover(url string)  (string, error) {
+func WatermarkRemover(url string) (string, error) {
 	id, err := getVideoId(url)
 	if err != nil {
 		log.Fatal(err)
 		return "", err
 	}
+
+	if len(id) == 0 {
+		return "", nil
+	}
+
 	videoLink, err := getVideoLink(id)
 
 	if err != nil {
@@ -15,11 +20,24 @@ func WatermarkRemover(url string)  (string, error) {
 		return "", err
 	}
 
+	if len(videoLink) == 0 {
+		return "", nil
+	}
+
+
+
 	finalLink, err := getFinalVideoLink(videoLink)
+
 	if err != nil {
 		log.Fatal(err)
 		return "", err
 	}
+
+	if len(finalLink) == 0 {
+		return "", nil
+	}
+
+
+	
 	return finalLink, nil
 }
-
