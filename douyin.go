17530283fb8429cd08bd2d6ba424d8645a922123
Roger Lee <rogerlee0421@gmail.com>
2020-09-18 10:07:49 +0800

    Update douyin.go

--- douyin.go
+++ douyin.go
@@ -5,16 +5,19 @@ import (
 )
 
 func WatermarkRemover(url string) (string, error) {
+	// 先获取视频的ID
 	id, err := getVideoId(url)
 	if err != nil {
 		log.Fatal(err)
 		return "", err
 	}
 
+	// 如果获取不到就返回
 	if len(id) == 0 {
 		return "", nil
 	}
 
+	// 根据ID获取视频信息
 	videoLink, err := getVideoLink(id)
 
 	if err != nil {
